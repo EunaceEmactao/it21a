317da60c9b7a0e29e730edaf071f8d39c15e5f4a
Eunace Faith Emactao <kimeunna.917@gmail.com>
2025-09-08 09:01:52 +0800

    added backtoMenu function

--- factorial.js
+++ factorial.js
@@ -17,8 +17,9 @@ console.log("3. Exit");
 rl.question("Enter your choice (1-3)", choice => {
 if(choice === "1"){
     console.log("Hello There");
+    backtoMenu();
+
 }else if(choice === "2"){
-    console.log("Factorial");
     computeFactorial();
 
 } else if(choice==="3"){
@@ -30,13 +31,13 @@ if(choice === "1"){
 }else{
     console.log("Invalid choice. Please try again.");
     mainMenu();
-}
+} 
 })
 } 
 
 
  function computeFactorial(){
-    
+     console.log("Factorial");
     rl.question("Enter a number for factorial: ", (numStr) =>{
     let num = parseInt(numStr);
 
@@ -53,7 +54,28 @@ console.log("Please enter a non-negative integer.");
 console.log("The factorial of "+ num + " is = " + fact);
 
     }
+    backtoMenu(); 
 });
+ }
+
+ function backtoMenu(){
+    console.log("----------");
+    console.log("1. Back to menu");
+    console.log("2. Exit");
+
+    rl.question("what would you like to do next (1-2)?: ", (backtoMenuChoice) =>{
+        if(backtoMenuChoice === "1"){
+            console.clear();
+            mainMenu();
+        }else if (backtoMenuChoice === "2"){
+            console.log("Exiting program... Goodbye!");
+            rl.close();
+        }else{
+            console.log("Invalid choice. Please try again.");
+            backtoMenu();
+        
+        }
+    });
  }
 
  mainMenu();

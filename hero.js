3af358e8769add93da83dd8b3f9e9732f0f2920b
Eunace Faith Emactao <kimeunna.917@gmail.com>
2025-09-18 08:37:09 +0800

    added addItem(), totalAttack()

--- hero.js
+++ hero.js
@@ -10,12 +10,23 @@ class Hero{
         this.name = name;
         this.#health = health;
         this.attack = attack;
+        this.items = [];
     
 
     }
 getHealth(){
     return this.#health;
 }
+
+getAttack(){
+    console.log(this.attack);
+
+}
+ getName(){
+    console.log(this.name);
+ }
+
+
 //inheritance example 
     getStats()  {
         console.log("\n");
@@ -23,6 +34,16 @@ getHealth(){
         console.log("Health:" + this.#health);
         console.log("Attack:" + this.attack);
     }
+
+
+addItem(item){
+    this.items.push(item);
+
+}
+//return: ibalik ang actual nga value 
+totalAttack(){
+    return this.attack + this.items.reduce((sum, item) => sum + item.bonusAttack, 0);
+}
 }
 
 
@@ -42,18 +63,39 @@ useAbility(){
 }
 } 
 
+class Item{
+constructor(name, bonusAttack) {
+    this.name = name;
+    this.bonusAttack = bonusAttack; 
+}
+}
+
+const sword = new Item("Sword", 5);
+const staff = new Item("Staff", 3); 
+
 
 
 const BlackPanther = new Warrior("Black Panther", 100, 50);
 //BlackPanther.useAbility();
-BlackPanther.getStats();    
+//BlackPanther.getStats();    
+
+BlackPanther.getName(); 
+BlackPanther.getAttack();
+
+
+
+BlackPanther.addItem(sword);
+console.log(BlackPanther.totalAttack());
+BlackPanther.addItem(sword);
+console.log(BlackPanther.totalAttack());
+
 
 const DoctorStrange = new Mage("Doctor Strange", 80, 8, 50);
 //DoctorStrange.useAbility();
-DoctorStrange.getStats();
+//DoctorStrange.getStats();
 
 
-//abstraction example
+//abstraction example; ginagamit for security purposes
 function performAbility(hero){
 console.log("\n");
     hero.useAbility();
